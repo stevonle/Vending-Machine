b0a1095ecc567d3a68c3b4dc5c30df212b02ff1c
Daniel Commins <danel.commins@techelevator.com>
2022-11-16 21:58:26 -0800

    module 1 capstone starter initial commit

---
+++ src/main/java/com/techelevator/ui/UserInput.java
@@ -0,0 +1,50 @@
+package com.techelevator.ui;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+/**
+ * Responsibilities: This class should handle receiving ALL input from the User
+ * 
+ * Dependencies: None
+ */
+public class UserInput
+{
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static String getHomeScreenOption()
+    {
+        System.out.println("What would you like to do?");
+        System.out.println();
+
+        System.out.println("D) Display Vending Machine Items");
+        System.out.println("P) Purchase");
+        System.out.println("E) Exit");
+
+        System.out.println();
+        System.out.print("Please select an option: ");
+
+        String selectedOption = scanner.nextLine();
+        String option = selectedOption.trim().toUpperCase();
+
+        if (option.equals("D"))
+        {
+            return "display";
+        }
+        else if (option.equals("P"))
+        {
+            return "purchase";
+        }
+        else if (option.equals("E"))
+        {
+            return "exit";
+        }
+        else
+        {
+            return "";
+        }
+
+    }
+
+    
+}
